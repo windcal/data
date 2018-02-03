34fcf8eb334ce1434ddcd8e4216ff16255e1bdb8
windcal <happylim1978@gmail>
2018-02-04 00:24:10 +0900

    v1.2
    v1.2

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+var express = require('express');
+var app = express();
+app.get('/', function(rep, res){
+    res.send('home');
 });
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(3000, function(){
+    console.log('3000 port')
+});
