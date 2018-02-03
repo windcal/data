b8608432accba74624b196b7940fe5b0d8d22c46
windcal <happylim1978@gmail>
2018-02-04 00:47:38 +0900

    v1.4
    v1.4

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
-app.get('/', function(rep, res){
-    res.send('home');
+var router = express.Router();
+
+router.get('/things', function(req, res) {
+    res.json('GET route on things.');
 });
-app.listen(3000, function(){
-    console.log('3000 port')
+router.post('/things', function(req, res) {
+    res.json('POST route on things.');
 });
+
+app.get('/hello', function(req, res) {
+    res.json("Hello World!");
+});
+
+app.post('/hello', function(req, res) {
+    res.json("You just called the post method at '/hello'!\n");
+});
+
+const port = process.env.PORT || 1337;
+app.listen(port);
